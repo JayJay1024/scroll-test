1c3933aa678744beb07330ae8a274b2b6efaccd1
JayJay1024 <lygm1062@gmail.com>
2024-02-01 17:48:22 +0800

    ci: change deploy by vercel

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
-  images: { unoptimized: true },
-  experimental: { images: { unoptimized: true } },
-  basePath: "/scroll-test",
+  // output: "export",
+  // images: { unoptimized: true },
+  // experimental: { images: { unoptimized: true } },
+  // basePath: "/scroll-test",
 };
 
 export default nextConfig;
